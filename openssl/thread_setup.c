b2cd61679016dd2801aa322eae2aa99f9ae9531a
Quim Muntal <quimmuntal@gmail.com>
2022-09-23 06:52:22 +0000

    pr feedback

--- openssl/thread_setup.c
+++ openssl/thread_setup.c
@@ -11,18 +11,17 @@
 #define _GNU_SOURCE
 #include <unistd.h>
  
-#define CRYPTO_LOCK      0x01
+#define CRYPTO_LOCK 0x01
 
 /* This array will store all of the mutexes available to OpenSSL. */ 
 static pthread_mutex_t *mutex_buf = NULL;
  
 static void locking_function(int mode, int n, const char *file, int line)
 {
-    if(mode & CRYPTO_LOCK) {
+    if(mode & CRYPTO_LOCK)
         pthread_mutex_lock(&mutex_buf[n]);
-    } else {
+    else
         pthread_mutex_unlock(&mutex_buf[n]);
-    }
 }
  
 static unsigned long id_function(void)
@@ -33,13 +32,11 @@ static unsigned long id_function(void)
 int go_openssl_thread_setup(void)
 {
     mutex_buf = malloc(go_openssl_CRYPTO_num_locks()*sizeof(pthread_mutex_t));
-    if(!mutex_buf) {
+    if(!mutex_buf)
         return 0;
-    }
     int i;
-    for(i = 0;  i < go_openssl_CRYPTO_num_locks(); i++) {
+    for(i = 0;  i < go_openssl_CRYPTO_num_locks(); i++)
         pthread_mutex_init(&mutex_buf[i], NULL);
-    }
     go_openssl_CRYPTO_set_id_callback(id_function);
     go_openssl_CRYPTO_set_locking_callback(locking_function);
     return 1;
