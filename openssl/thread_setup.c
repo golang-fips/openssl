e96d8a873e56064af5fa76a686e25944cec61c3c
Quim Muntal <quimmuntal@gmail.com>
2022-09-24 18:56:17 +0200

    Apply suggestions from code review
    Co-authored-by: Davis Goodin <dagood@users.noreply.github.com>

--- openssl/thread_setup.c
+++ openssl/thread_setup.c
@@ -18,7 +18,7 @@ static pthread_mutex_t *mutex_buf = NULL;
  
 static void locking_function(int mode, int n, const char *file, int line)
 {
-    if(mode & CRYPTO_LOCK)
+    if (mode & CRYPTO_LOCK)
         pthread_mutex_lock(&mutex_buf[n]);
     else
         pthread_mutex_unlock(&mutex_buf[n]);
@@ -32,10 +32,10 @@ static unsigned long id_function(void)
 int go_openssl_thread_setup(void)
 {
     mutex_buf = malloc(go_openssl_CRYPTO_num_locks()*sizeof(pthread_mutex_t));
-    if(!mutex_buf)
+    if (!mutex_buf)
         return 0;
     int i;
-    for(i = 0;  i < go_openssl_CRYPTO_num_locks(); i++)
+    for (i = 0; i < go_openssl_CRYPTO_num_locks(); i++)
         pthread_mutex_init(&mutex_buf[i], NULL);
     go_openssl_CRYPTO_set_id_callback(id_function);
     go_openssl_CRYPTO_set_locking_callback(locking_function);
