46e5f7e3596f69284e68f8a4a1e7511681715b49
Quim Muntal <quimmuntal@gmail.com>
2022-10-06 08:43:01 +0200

    Apply suggestions from code review
    Co-authored-by: Davis Goodin <dagood@users.noreply.github.com>

--- openssl/rand.go
+++ openssl/rand.go
@@ -13,7 +13,6 @@ func (randReader) Read(b []byte) (int, error) {
 	// Note: RAND_bytes should never fail; the return value exists only for historical reasons.
 	// We check it even so.
 	if len(b) > 0 && C.go_openssl_RAND_bytes((*C.uchar)(unsafe.Pointer(&b[0])), C.int(len(b))) == 0 {
-		// TODO: use NewOpenSSLError once implemented.
 		return 0, newOpenSSLError("RAND_bytes")
 	}
 	return len(b), nil
