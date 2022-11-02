d1cf9055481d50207d71b42feb28d1692faa6259
Quim Muntal <quimmuntal@gmail.com>
2022-11-02 09:08:24 +0000

    code review feedback

--- openssl/port_evp_md5_sha1.c
+++ openssl/port_evp_md5_sha1.c
@@ -15,13 +15,20 @@
 
 #define NID_md5_sha1            114
 
-# define MD5_CBLOCK      64
-# define MD5_LBLOCK      (MD5_CBLOCK/4)
-# define MD5_DIGEST_LENGTH 16
-# define SHA_LBLOCK      16
-# define SHA_DIGEST_LENGTH 20
+#define MD5_CBLOCK      64
+#define MD5_LBLOCK      (MD5_CBLOCK/4)
+#define MD5_DIGEST_LENGTH 16
+#define SHA_LBLOCK      16
+#define SHA_DIGEST_LENGTH 20
 
-#  define EVP_PKEY_NULL_method    NULL,NULL,{0,0,0,0}
+#define EVP_PKEY_NULL_method    NULL,NULL,{0,0,0,0}
+
+// Change: MD5_LONG and SHA_LONG have been expanded to unsigned int,
+// which is always 32 bits. This avoids adding some obscure logic
+// to support 16-bit platforms.
+
+# define MD5_LONG unsigned int
+# define SHA_LONG unsigned int
 
 typedef struct env_md_st EVP_MD;
 typedef struct env_md_ctx_st EVP_MD_CTX;
@@ -54,44 +61,44 @@ struct env_md_st {
 } /* EVP_MD */ ;
 
 typedef struct MD5state_st {
-    unsigned int A, B, C, D;
-    unsigned int Nl, Nh;
-    unsigned int data[MD5_LBLOCK];
-    unsigned int num;
+    MD5_LONG A, B, C, D;
+    MD5_LONG Nl, Nh;
+    MD5_LONG data[MD5_LBLOCK];
+    MD5_LONG num;
 } MD5_CTX;
 
 typedef struct SHAstate_st {
-    unsigned int h0, h1, h2, h3, h4;
-    unsigned int Nl, Nh;
-    unsigned int data[SHA_LBLOCK];
-    unsigned int num;
+    SHA_LONG h0, h1, h2, h3, h4;
+    SHA_LONG Nl, Nh;
+    SHA_LONG data[SHA_LBLOCK];
+    SHA_LONG num;
 } SHA_CTX;
 
 struct md5_sha1_ctx {
-  MD5_CTX md5;
-  SHA_CTX sha1;
+    MD5_CTX md5;
+    SHA_CTX sha1;
 };
 
 static int md5_sha1_init(EVP_MD_CTX *ctx) {
-  struct md5_sha1_ctx *mctx = ctx->md_data;
-  if (!go_openssl_MD5_Init(&mctx->md5))
-    return 0;
-  return go_openssl_SHA1_Init(&mctx->sha1);
+    struct md5_sha1_ctx *mctx = ctx->md_data;
+    if (!go_openssl_MD5_Init(&mctx->md5))
+        return 0;
+    return go_openssl_SHA1_Init(&mctx->sha1);
 }
 
 static int md5_sha1_update(EVP_MD_CTX *ctx, const void *data,
                                            size_t count) {
-  struct md5_sha1_ctx *mctx = ctx->md_data;
-  if (!go_openssl_MD5_Update(&mctx->md5, data, count))
-    return 0;
-  return go_openssl_SHA1_Update(&mctx->sha1, data, count);
+    struct md5_sha1_ctx *mctx = ctx->md_data;
+    if (!go_openssl_MD5_Update(&mctx->md5, data, count))
+        return 0;
+    return go_openssl_SHA1_Update(&mctx->sha1, data, count);
 }
 
 static int md5_sha1_final(EVP_MD_CTX *ctx, unsigned char *md) {
-  struct md5_sha1_ctx *mctx = ctx->md_data;
-  if (!go_openssl_MD5_Final(md, &mctx->md5))
-    return 0;
-  return go_openssl_SHA1_Final(md + MD5_DIGEST_LENGTH, &mctx->sha1);
+    struct md5_sha1_ctx *mctx = ctx->md_data;
+    if (!go_openssl_MD5_Final(md, &mctx->md5))
+        return 0;
+    return go_openssl_SHA1_Final(md + MD5_DIGEST_LENGTH, &mctx->sha1);
 }
 
 // Change: Removed:
@@ -115,5 +122,5 @@ static const EVP_MD md5_sha1_md = {
 
 // Change: Apply name mangling.
 const GO_EVP_MD_PTR go_openssl_EVP_md5_sha1_backport(void) {
-  return (const GO_EVP_MD_PTR)&md5_sha1_md;
+    return (const GO_EVP_MD_PTR)&md5_sha1_md;
 }
