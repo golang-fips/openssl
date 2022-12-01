bd12af7eaee5cd2a91fe907ace4f9b93797dbe4c
Quim Muntal <quimmuntal@gmail.com>
2022-12-01 19:04:05 +0000

    add commit hash

--- openssl/bbig/big.go
+++ openssl/bbig/big.go
@@ -2,7 +2,8 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// This is a mirror of crypto/internal/boring/bbig/big.go.
+// This is a mirror of
+// https://github.com/golang/go/blob/36b87f273cc43e21685179dc1664ebb5493d26ae/src/crypto/internal/boring/bbig/big.go.
 
 package bbig
 
