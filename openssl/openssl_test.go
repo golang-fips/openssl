55d95d85403f120d191073ca028e140681d4fe77
Daiki Ueno <dueno@redhat.com>
2023-04-25 08:39:02 +0900

    Make use of go build -asan to detect memory issues
    Go 1.18 has added the `-asan` option to `go build`, which can be used
    to find memory related programming issues.  To catch memory leaks,
    this patch manually calls `__lsan_do_leak_check` after running GC in
    `TestMain`.
    
    Signed-off-by: Daiki Ueno <dueno@redhat.com>

---
+++ openssl/openssl_test.go
@@ -0,0 +1,20 @@
+//go:build linux && !android
+// +build linux,!android
+
+package openssl_test
+
+import (
+	"os"
+	"runtime"
+	"testing"
+
+	"github.com/golang-fips/openssl-fips/openssl"
+)
+
+
+func TestMain(m *testing.M) {
+	exitVal := m.Run()
+	runtime.GC()
+	openssl.CheckLeaks()
+	os.Exit(exitVal)
+}
