3f800cf4bcf93a623ec098591049953d7ed65391
qmuntal <qmuntaldiaz@microsoft.com>
2024-09-03 09:02:20 +0200

    improve port_dsa.c description

--- port_dsa.c
+++ port_dsa.c
@@ -1,6 +1,7 @@
-// The following is a partial backport of crypto/dsa/dsa_pmeth.c,
-// commit cbc8a839959418d8a2c2e3ec6bdf394852c9501e on the
-// OpenSSL_1_1_0-stable branch.  The ctrl function has been removed.
+// The following is a partial backport of crypto/dsa/dsa_lockl.h
+// and crypto/dsa/dsa_lib.c, commit cbc8a839959418d8a2c2e3ec6bdf394852c9501e
+// on the OpenSSL_1_1_0-stable branch. Only pqg and key getters/setters
+// are backported.
 
 #include "goopenssl.h"
 
@@ -14,6 +15,8 @@ struct dsa_st
     GO_BIGNUM_PTR g;
     GO_BIGNUM_PTR pub_key;
     GO_BIGNUM_PTR priv_key;
+    // The following members are not used by our backport,
+    // so we don't define them here.
 };
 
 void go_openssl_DSA_get0_pqg_backport(const GO_DSA_PTR dsa,
