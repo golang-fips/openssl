c9b7342d912368efc2f328a64784fc7dec7c35ee
qmuntal <quimmuntal@gmail.com>
2024-09-04 14:23:26 +0000

    SymCryptProviderAvailable should always return false in OpenSSL 1

--- export_test.go
+++ export_test.go
@@ -5,5 +5,8 @@ import "sync"
 var ErrOpen = errOpen
 
 var SymCryptProviderAvailable = sync.OnceValue(func() bool {
+	if vMajor == 1 {
+		return false
+	}
 	return isProviderAvailable("symcryptprovider")
 })
