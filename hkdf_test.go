1c95ba95806d8b9e342b072a8850f494f172888d
qmuntal <qmuntaldiaz@microsoft.com>
2024-09-23 10:15:03 +0200

    add continue in TestHKDF

--- hkdf_test.go
+++ hkdf_test.go
@@ -317,6 +317,7 @@ func TestHKDF(t *testing.T) {
 		hkdf, err := newHKDF(tt.hash, tt.master, tt.salt, tt.info)
 		if err != nil {
 			t.Errorf("test %d: error creating HKDF: %v.", i, err)
+			continue
 		}
 		out := make([]byte, len(tt.out))
 
