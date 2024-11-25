83fb30f6f91c4e8e382717e4329102ea57b53eef
qmuntal <qmuntaldiaz@microsoft.com>
2024-11-25 20:55:11 +0100

    add ECDH key validations for OpenSSL 3

--- ecdh.go
+++ ecdh.go
@@ -205,7 +205,20 @@ func newECDHPkey3(nid C.int, bytes []byte, isPrivate bool) (C.GO_EVP_PKEY_PTR, e
 		return nil, err
 	}
 	defer C.go_openssl_OSSL_PARAM_free(params)
-	return newEvpFromParams(C.GO_EVP_PKEY_EC, selection, params)
+	pkey, err := newEvpFromParams(C.GO_EVP_PKEY_EC, selection, params)
+	if err != nil {
+		return nil, err
+	}
+	if isPrivate {
+		if C.go_openssl_EVP_PKEY_private_check(pkey) != 1 {
+			return nil, errors.New("crypto/ecdh: invalid private key")
+		}
+	} else {
+		if C.go_openssl_EVP_PKEY_public_check_quick(pkey) != 1 {
+			return nil, errors.New("crypto/ecdh: invalid public key")
+		}
+	}
+	return pkey, nil
 }
 
 func pointMult(group C.GO_EC_GROUP_PTR, priv C.GO_BIGNUM_PTR) (C.GO_EC_POINT_PTR, error) {
