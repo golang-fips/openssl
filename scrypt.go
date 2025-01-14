b360de8f47f2541fc3303c799b5efb44565992b1
Harry Brown <harrybrown98@gmail.com>
2025-01-13 21:47:56 -0800

    added wrapper for EVP_BPE_scrypt

---
+++ scrypt.go
@@ -0,0 +1,37 @@
+//go:build !cmd_go_bootstrap
+
+package openssl
+
+// #include "goopenssl.h"
+import "C"
+import (
+	"unsafe"
+)
+
+func Scrypt(password string, salt []byte, N, r, p, maxmem, keylen uint64) ([]byte, error) {
+	cpassword := C.CString(password)
+	defer C.free((unsafe.Pointer)(cpassword))
+
+	csalt := C.CBytes(salt)
+	defer C.free((unsafe.Pointer)(csalt))
+
+	key := C.malloc(C.size_t(keylen))
+	defer C.free(key)
+
+	res := C.go_openssl_EVP_PBE_scrypt(
+		cpassword,
+		C.size_t(len(password)),
+		(*C.uchar)(csalt),
+		C.size_t(len(salt)),
+		C.uint64_t(N),
+		C.uint64_t(r),
+		C.uint64_t(p),
+		C.uint64_t(maxmem),
+		(*C.uchar)(key),
+		C.size_t(keylen),
+	)
+	if res != 1 {
+		return nil, newOpenSSLError("EVP_PBE_scrypt")
+	}
+	return C.GoBytes(key, C.int(keylen)), nil
+}
