c80a17c057fcb4a28870386b00b2ecea0e0fce61
qmuntal <qmuntaldiaz@microsoft.com>
2025-09-18 15:51:53 +0200

    fix windows

--- internal/ossl/syscall_windows_nocgo.go
+++ internal/ossl/syscall_windows_nocgo.go
@@ -1,13 +1,25 @@
-//go:build !cgo && windows
+//go:build windows && !cgo
 
 package ossl
 
 import (
 	"syscall"
+	"unsafe"
 )
+
+var modkernel32 = syscall.NewLazyDLL("kernel32.dll")
+var procGetProcAddress = modkernel32.NewProc("GetProcAddress")
 
 //go:nosplit
 func syscallN(fn uintptr, args ...uintptr) (r1, r2 uintptr, err syscall.Errno) {
 	r1, r2, err = syscall.SyscallN(fn, args...)
 	return
+}
+
+func Dlsym(handle unsafe.Pointer, symbol *byte) (unsafe.Pointer, error) {
+	r0, _, err := syscall.SyscallN(procGetProcAddress.Addr(), uintptr(handle), uintptr(unsafe.Pointer(symbol)))
+	if err != 0 {
+		return nil, err
+	}
+	return unsafe.Pointer(r0), nil
 }
