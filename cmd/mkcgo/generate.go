661ae7104dfbe7f2ca1e1908491be941dc767511
qmuntal <quimmuntal@gmail.com>
2025-09-22 09:45:24 +0200

    fix type map

--- cmd/mkcgo/generate.go
+++ cmd/mkcgo/generate.go
@@ -462,7 +462,7 @@ var cstdTypesToGo = map[string]string{
 	"int":                "int32",
 	"unsigned":           "uint32",
 	"unsigned int":       "uint32",
-	"long":               "uint32",
+	"long":               "int32",
 	"unsigned long":      "uint32",
 	"long long":          "int64",
 	"unsigned long long": "uint64",
