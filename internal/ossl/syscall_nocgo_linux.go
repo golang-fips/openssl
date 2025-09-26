aad5524f1090ad0fe80acdcd30a84002743178c9
Quim Muntal <quimmuntal@gmail.com>
2025-09-26 12:16:02 +0000

    support cgoless linux and darwin

--- internal/ossl/syscall_nocgo_linux.go
+++ internal/ossl/syscall_nocgo_linux.go
@@ -2,11 +2,4 @@
 
 package ossl
 
-import "syscall"
-
 //go:cgo_import_dynamic _ _ "libdl.so.2"
-
-// TODO implement syscallN for Linux nocgo
-func syscallN(fn uintptr, args ...uintptr) (r1, r2 uintptr, err syscall.Errno) {
-	panic("Syscall not implemented on Linux")
-}
