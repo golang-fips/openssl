bc5341a041a2d360a5aff175e4dc730bac6f7b69
qmuntal <quimmuntal@gmail.com>
2025-10-21 10:30:36 +0200

    do error handling on the system stack

--- internal/ossl/syscall_nocgo_windows.go
+++ internal/ossl/syscall_nocgo_windows.go
@@ -10,12 +10,6 @@ import (
 var modkernel32 = syscall.NewLazyDLL("kernel32.dll")
 var procGetProcAddress = modkernel32.NewProc("GetProcAddress")
 
-//go:nosplit
-func syscallN(fn uintptr, args ...uintptr) (r1, r2 uintptr, err syscall.Errno) {
-	r1, r2, err = syscall.SyscallN(fn, args...)
-	return
-}
-
 func dlsym(handle unsafe.Pointer, symbol string, optional bool) uintptr {
 	r0, _, err := syscall.SyscallN(procGetProcAddress.Addr(), uintptr(handle), uintptr(unsafe.Pointer(unsafe.StringData(symbol))))
 	if err != 0 {
