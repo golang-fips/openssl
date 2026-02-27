5a58e1a880973542c00af9ec2c927c86829caa8d
michelle-clayton-work <mclayton+github@microsoft.com>
2026-02-27 12:04:43 -0800

    Update internal/ossl/syscall_nocgo.go

--- internal/ossl/syscall_nocgo.go
+++ internal/ossl/syscall_nocgo.go
@@ -23,7 +23,7 @@ var escapeSink unsafe.Pointer
 
 // escapePtr forces p to escape to the heap.
 // This implementation is also used in the standard library:
-// https://github.com/golang/go/blob/go1.26.0/src/internal/abi/escape.go#L24-L33
+// https://github.com/golang/go/blob/f71432d223eeb2139b460957817400750fd13655/src/internal/abi/escape.go#L24-L33
 func escapePtr(p unsafe.Pointer) unsafe.Pointer {
 	if alwaysFalse {
 		escapeSink = p
