1cb69965c4b0b46dcd7b0b02fdb4927d3cfcfe37
Davis Goodin <dagood@microsoft.com>
2026-03-02 09:38:31 -0800

    GOOS=linux GOARCH=arm gotip fix ./... (modified go)

--- openssl_nocgo.go
+++ openssl_nocgo.go
@@ -11,7 +11,7 @@ func goString(ptr *byte) string {
 	}
 	var result []byte
 	for i := 0; ; i++ {
-		b := *(*byte)(unsafe.Pointer(uintptr(unsafe.Pointer(ptr)) + uintptr(i)))
+		b := *(*byte)(unsafe.Add(unsafe.Pointer(ptr), i))
 		if b == 0 {
 			break
 		}
