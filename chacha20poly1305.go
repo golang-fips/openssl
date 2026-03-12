bb617464cb0515c9885fd62fccd5d27253d9ba23
qmuntal <quimmuntal@gmail.com>
2026-03-12 09:21:09 +0100

    support mkcgo slices with lenght pointer

--- chacha20poly1305.go
+++ chacha20poly1305.go
@@ -85,7 +85,7 @@ func (c *chacha20poly1305) Seal(dst, nonce, plaintext, additionalData []byte) []
 			panic(err)
 		}
 	}
-	if _, err := ossl.EVP_EncryptFinal_ex(ctx, base(out[outl:]), &outl); err != nil {
+	if _, err := ossl.EVP_EncryptFinal_ex(ctx, out[outl:], &outl); err != nil {
 		panic(err)
 	}
 	tag := out[len(out)-chacha20Poly1305Overhead:]
@@ -141,7 +141,7 @@ func (c *chacha20poly1305) Open(dst, nonce, ciphertext, additionalData []byte) (
 			return nil, err
 		}
 	}
-	if _, err := ossl.EVP_DecryptFinal_ex(ctx, base(out[outl:]), &outl); err != nil {
+	if _, err := ossl.EVP_DecryptFinal_ex(ctx, out[outl:], &outl); err != nil {
 		return nil, errOpen
 	}
 	runtime.KeepAlive(c)
