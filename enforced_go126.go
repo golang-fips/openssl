99eb8f9555ca693568e38e709658e8555169f25d
Dimitri John Ledkov <dimitri.ledkov@surgut.co.uk>
2026-05-18 15:05:40 +0100

    Always set fips params

--- enforced_go126.go
+++
@@ -1,7 +0,0 @@
-//go:build !cmd_go_bootstrap && go1.26
-
-package openssl
-
-import "crypto/fips140"
-
-var fips140Enforced = fips140.Enforced
--- enforced_nogo126.go
+++
@@ -1,7 +0,0 @@
-//go:build !cmd_go_bootstrap && !go1.26
-
-package openssl
-
-func fips140Enforced() bool {
-	return true
-}
